8114e4b9147197ded13f5b678d5633be6257834a
ваше_имя <_@email.com>
2023-01-21 09:00:24 +0300

    more stuff

---
+++ internal/models/user.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+func (user *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+func (user *User) ComparePassword(password string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return err
+	}
+	return nil
+}
